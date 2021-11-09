da34cc5df8574acc02590e1844ed5876ebd8b769
Danil <korotkin321@gmail.com>
2021-11-09 13:46:39 +0300

    Made the changes written in the task

--- src/main/java/koschei/models/Wood3.java
+++ src/main/java/koschei/models/Wood3.java
@@ -6,15 +6,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class Wood3 {
 
-    private final Rabbit4 rabbit;
-
-    @Autowired
-    public Wood3(Rabbit4 rabbit) {
-        this.rabbit = rabbit;
-    }
+    private Rabbit4 rabbit;
 
     @Override
     public String toString() {
         return ", на дереве заяц " + rabbit.toString();
+    }
+
+    @Autowired
+    public void setRabbit(Rabbit4 rabbit) {
+        this.rabbit = rabbit;
     }
 }
